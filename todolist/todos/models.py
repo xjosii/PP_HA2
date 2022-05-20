3118bb38d35b279b858cea50a1f394b820d2bdcd
florian-vogel <fl_vogel@web.de>
2022-05-20 02:37:17 +0200

    add button

--- todolist/todos/models.py
+++ todolist/todos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+# TODO: change datatype
 class ListEntry(models.Model):
     task = models.CharField(max_length=160)
     deadline = models.CharField(max_length=10)

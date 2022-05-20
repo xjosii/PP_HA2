d6647fb7d5d8c0eff9df4006242ed656c272b205
florian-vogel <fl_vogel@web.de>
2022-05-20 15:18:15 +0200

    alter model type

--- todolist/todos/models.py
+++ todolist/todos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # TODO: change datatype
+
+
 class ListEntry(models.Model):
     task = models.CharField(max_length=160)
     deadline = models.CharField(max_length=10)
-    progress = models.CharField(max_length=5)
+    progress = models.IntegerField(default=0)
